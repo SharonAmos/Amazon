d97175ad868c3a9dac2f7f22b54cb4205ad11434
SharonAmos <sharonchristjesus@gmail.com>
2021-06-30 15:52:37 +0530

    Git first Push

---
+++ src/org/junegit/EmployeeDetails.java
@@ -0,0 +1,16 @@
+package org.junegit;
+
+public class EmployeeDetails {
+	
+		private void java() {
+			System.out.println("java");
+
+		}
+		public static void main(String[] args) {
+			EmployeeDetails e = new EmployeeDetails();
+			e.java();
+		}
+
+	}
+
+
